495279b39c27cb7fc66b6c7a8a91f6a264167a29
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-09-07 13:40:48 +0500

    end

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-setTimeout(() => {
-    const ism = prompt("Ismingizni kiriting:");
-    alert(`Salom, ${ism}!`);
-}, 3000); 
+document.addEventListener('DOMContentLoaded', () => {
+    const message = document.getElementById('message');
+    let intervalCount = 0;
 
-const intervalId = setInterval(() => {
-    console.log("Bu xabar har 2 soniyada bir marta ko'rsatiladi.");
-}, 2000); 
+    const colors = ['red', 'blue', 'green', 'yellow', 'purple','orange','aqua'];
+    const intervalId = setInterval(() => {
 
-setTimeout(() => {
-    clearInterval(intervalId);
-    console.log("Interval to'xtatildi.");
-}, 10000); 
+        message.style.color = colors[intervalCount % colors.length];
+        intervalCount += 1;
+
+        if (intervalCount >= 7) {
+            clearInterval(intervalId); 
+            message.style.color = 'green'; 
+            console.log("Animatsiya to'xtatildi, rang yashilga o'zgardi.");
+            setTimeout(() => {
+                console.log("Animatsiya tugadi!");
+            }, 2000);
+        }
+    }, 1000);
+});
